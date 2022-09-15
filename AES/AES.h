1ed72a69eb556bbe4612c4a6144acc2b76bf12e8
Yong Jin Lee <yjtarzan@kookmin.ac.kr>
2022-09-16 02:22:03 +0900

    AES128/192/256 Encryption

--- AES/AES.h
+++ AES/AES.h
@@ -6,3 +6,18 @@ typedef unsigned char uint8;
 
 #define BlockSize 16
 
+void RotWord(uint8* t);
+void SubWord(uint8* t);
+void RotSubWord(uint8* t);
+
+uint8 xtime(uint8 a);
+uint8 m02(uint8 a);
+uint8 m03(uint8 a);
+
+void AddRoundKey(uint8* arr, uint8* key, uint8 Round);
+void SubBytes(uint8* arr);
+void ShiftRows(uint8* arr);
+void MixColumns(uint8* arr);
+
+uint8* KeySchedule(uint8* key, uint8 size);
+void AES_Encryption(uint8* plain, uint8* key, uint8 keySize, uint8* cipher);
