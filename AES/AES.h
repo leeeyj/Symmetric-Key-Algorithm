6d9321092ddec902a83a282dc9d6840e534b5a9f
Yong Jin Lee <yjtarzan@kookmin.ac.kr>
2022-09-16 13:07:41 +0900

    AES 128/192/256

--- AES/AES.h
+++ AES/AES.h
@@ -10,14 +10,29 @@ void RotWord(uint8* t);
 void SubWord(uint8* t);
 void RotSubWord(uint8* t);
 
-uint8 xtime(uint8 a);
+// Multiplication on GF(2^8) 
+uint8 xtime(uint8 a);   
 uint8 m02(uint8 a);
 uint8 m03(uint8 a);
+uint8 m04(uint8 a);
+uint8 m08(uint8 a);
+uint8 m09(uint8 a);
+uint8 m0b(uint8 a);
+uint8 m0d(uint8 a);
+uint8 m0e(uint8 a);
 
+// AES basic function 
 void AddRoundKey(uint8* arr, uint8* key, uint8 Round);
 void SubBytes(uint8* arr);
+void InvSubBytes(uint8* arr);
 void ShiftRows(uint8* arr);
+void InvShiftRows(uint8* arr);
 void MixColumns(uint8* arr);
+void InvMixColumns(uint8* arr);
 
+// AES KeySchedule  
 uint8* KeySchedule(uint8* key, uint8 size);
+
+// AES operation function 
 void AES_Encryption(uint8* plain, uint8* key, uint8 keySize, uint8* cipher);
+void AES_Decryption(uint8* cipher, uint8* key, uint8 keySize, uint8* plain);
